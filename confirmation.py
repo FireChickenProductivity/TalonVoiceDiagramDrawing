cca42b5d2613726b7c044d039e15b33761be1f25
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-11-27 12:17:02 -0700

    Add confirmation functionality

---
+++ confirmation.py
@@ -0,0 +1,60 @@
+from talon import Module, Context, imgui
+
+module = Module()
+module.tag('diagram_drawing_confirmation', desc = 'Activates are you sure commands')
+
+class ConfirmationState:
+    def __init__(self):
+        self.context = SingleTagContext('user.diagram_drawing_confirmation')
+    
+    def request_confirmation(self, message: str, on_confirmation, on_disconfirmation):
+        self.on_confirmation = on_confirmation
+        self.on_disconfirmation = on_disconfirmation
+        self.message = message
+        self.context.on()
+        gui.show()
+    
+    def confirm(self):
+        self.on_confirmation()
+        self.cleanup()
+    
+    def disconfirm(self):
+        self.on_disconfirmation()
+        self.cleanup()
+
+    def cleanup(self):
+        self.context.off()
+        self.on_confirmation = None
+        self.on_disconfirmation = None
+        self.message = None
+        gui.hide()
+        
+    def get_message(self) -> str:
+        return self.message
+
+class SingleTagContext:
+    def __init__(self, tag_name: str):
+        self.tag_name = tag_name
+        self.context = Context()
+    
+    def on(self):
+        self.context.tags = [self.tag_name]
+    
+    def off(self):
+        self.context.tags = []
+    
+confirmation = ConfirmationState()
+
+@imgui.open()
+def gui(gui: imgui.GUI):
+    gui.text(confirmation.get_message())
+
+@module.action_class
+class Actions:
+    def diagram_drawing_confirm():
+        ''''''
+        confirmation.confirm()
+    
+    def diagram_drawing_disconfirm():
+        ''''''
+        confirmation.disconfirm()
