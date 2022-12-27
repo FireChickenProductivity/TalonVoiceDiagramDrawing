f6f27bf7472cbc2fbdbe82600758e712eb50aae6
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-12-27 16:29:52 -0700

    Add line drawing functionality

--- curves.py
+++ curves.py
@@ -1,5 +1,6 @@
 from talon import Module, actions
 from .fire_chicken.mouse_position import MousePosition
+from .position_storage import main_position_storage
 from enum import Enum
 import math
 
@@ -16,6 +17,80 @@ def draw_curve_dashed(points):
     for dash_ending in range(dash_size, len(points), dash_size*2):
         dash_start = dash_ending - dash_size
         draw_points(points[dash_start:dash_ending])
+
+def compute_shifted_curve_points(points, horizontal_shift: int, vertical_shift: int):
+    if len(points) <= 2:
+        return points
+    start: MousePosition = points[0]
+    ending: MousePosition = points[-1]
+    boundary = Boundary(start, ending)
+    in_between_points = points[1: -1]
+    result = []
+    result.append(start)
+    for point in in_between_points:
+        horizontal = point.get_horizontal() + horizontal_shift
+        vertical = point.get_vertical() + vertical_shift
+        new_position = boundary.compute_position_within_boundaries(horizontal, vertical)
+        result.append(new_position)
+    result.append(ending)
+    return result
+
+def compute_line_points(start: MousePosition, ending: MousePosition):
+    if start.get_horizontal() != ending.get_horizontal():
+        return compute_none_vertical_line_points(start, ending)
+    return compute_vertical_line_points(start, ending)
+
+def compute_none_vertical_line_points(start: MousePosition, ending: MousePosition):
+    slope = (ending.get_vertical() - start.get_vertical())/(ending.get_horizontal() - start.get_horizontal())
+    range_object = compute_range(start.get_horizontal(), ending.get_horizontal())
+    points = []
+    for horizontal in range_object:
+        relative_horizontal = horizontal - start.get_horizontal()
+        relative_position: MousePosition = MousePosition(relative_horizontal, relative_horizontal*slope)
+        point_position = relative_position + start
+        points.append(point_position)
+    return points
+
+def compute_vertical_line_points(start: MousePosition, ending: MousePosition):
+    range_object = compute_range(start.get_vertical(), ending.get_vertical())
+    horizontal = start.get_horizontal()
+    points = []
+    for vertical in range_object:
+        position = MousePosition(horizontal, vertical)
+        points.append(position)
+    return points
+
+def compute_range(start: int, inclusive_ending: int):
+    increment = 1
+    if start > inclusive_ending:
+        increment = -1
+    range_result = range(start, inclusive_ending + 1, increment)
+    return range_result
+
+class Boundary:
+    def __init__(self, start: MousePosition, ending: MousePosition):
+        self.lower_vertical_boundary = min(start.get_vertical(), ending.get_vertical())
+        self.upper_vertical_boundary = max(start.get_vertical(), ending.get_vertical())
+        self.lower_horizontal_boundary = min(start.get_horizontal(), ending.get_horizontal())
+        self.upper_horizontal_boundary = max(start.get_horizontal(), ending.get_horizontal())
+
+    def compute_vertical_within_boundary(self, vertical: int):
+        return self._compute_coordinate_within_boundary(vertical, self.lower_vertical_boundary, self.upper_vertical_boundary)
+    
+    def compute_horizontal_within_boundary(self, horizontal: int):
+        return self._compute_coordinate_within_boundary(horizontal, self.lower_vertical_boundary, self.upper_horizontal_boundary)
+    
+    def _compute_coordinate_within_boundary(self, coordinate: int, minimum: int, maximum: int):
+        result = coordinate
+        if coordinate < minimum:
+            result = minimum
+        elif coordinate > maximum:
+            result = maximum
+        return result
+
+    def compute_position_within_boundaries(self, horizontal: int, vertical: int) -> MousePosition:
+        position = MousePosition(self.compute_horizontal_within_boundary(horizontal), self.compute_vertical_within_boundary((vertical)))
+        return position
 
 class SwoopingDirection(Enum):
     UP = 1
@@ -74,3 +149,11 @@ class Actions:
         quadratic = Quadratic.from_positions_and_initial_slope(start, ending, initial_slope)
         points = quadratic.compute_points_between(start, ending)
         draw_curve_dashed(points)
+    
+    def diagram_drawing_draw_dashed_line_between_stored_positions(origin_position_number: int, destination_position_number: int):
+        ''''''
+        origin = main_position_storage.get_position_indexed_from_one(origin_position_number)
+        destination = main_position_storage.get_position_indexed_from_one(destination_position_number)
+        line_positions = compute_line_points(origin, destination)
+        draw_curve_dashed(line_positions)
+        
