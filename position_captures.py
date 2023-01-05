811fe27c406ee7e91810403566b5694fdb2632a1
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-01-05 14:52:22 -0700

    Add alpha grid coordinates

--- position_captures.py
+++ position_captures.py
@@ -4,7 +4,7 @@ from .fire_chicken.mouse_position import MousePosition
 PositionSpecifier = Union[int, List]
 
 module = Module()
-@module.capture(rule = '<number>|<user.diagram_drawing_number_float> by <user.diagram_drawing_number_float> [by <user.diagram_drawing_number_float>]')
+@module.capture(rule = '<number>|<user.diagram_drawing_number_float> by <user.diagram_drawing_number_float> [by <user.diagram_drawing_number_float>]|<user.letter> <user.letter>')
 def diagram_drawing_position_specifier(m) -> PositionSpecifier:
     ''''''
     try:
@@ -12,6 +12,11 @@ def diagram_drawing_position_specifier(m) -> PositionSpecifier:
     except:
         pass
     
+    try:
+        return m.letter_1 + m.letter_2
+    except: 
+        pass
+
     result = []
     for i in range(0, len(m), 2):
         result.append(m[i])
@@ -25,6 +30,8 @@ class Actions:
             return actions.user.diagram_drawing_get_position(specifier)
         elif type(specifier) == list:
             return actions.user.diagram_drawing_get_position_along_axes(*specifier)
+        elif type(specifier) == str:
+            return actions.user.diagram_drawing_get_alpha_grid_position(specifier)
         
     def diagram_drawing_move_mouse_to_position(specifier: PositionSpecifier):
         ''''''
