e3ae1016697bb9faf97349ace6fc7652a3a6756a
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-01-06 10:38:36 -0700

    Add actions for working with mouse positions

--- storage.py
+++ storage.py
@@ -1,6 +1,7 @@
 from talon import Module, actions
 from .fire_chicken import data_storage
 from .fire_chicken import path_utilities
+from .fire_chicken.mouse_position import MousePosition
 
 module = Module()
 @module.action_class
@@ -11,3 +12,19 @@ class Actions:
         data_directory = path_utilities.join_path(current_directory, 'data')
         storage = data_storage.Storage(data_directory)
         return storage
+    
+    def diagram_drawing_get_data_storage_position(name: str) -> MousePosition:
+        ''''''
+        position_file = get_data_storage_position_file(name)
+        position = position_file.get()
+        return position
+    
+    def diagram_drawing_set_data_storage_position_to_current_mouse_position(name: str):
+        ''''''
+        position_file = get_data_storage_position_file(name)
+        position_file.set_to_current_mouse_position()
+
+def get_data_storage_position_file(name: str) -> data_storage.MousePositionFile:
+    storage: data_storage.Storage = actions.user.diagram_drawing_compute_data_storage()
+    position_file = storage.get_position_file(name)
+    return position_file
